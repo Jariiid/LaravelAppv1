ecfdb9418ef4cbc9cd6255b2003cb13e2db57472
Jariiid <borjajared73@gmail.com>
2025-10-14 13:02:38 +0800

    Oct/14

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Chirp;
 
 class ChirpController extends Controller
 {
@@ -11,24 +12,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
-         $chirps = [
-        [
-            'author' => 'Jane Doe',
-            'message' => 'Just deployed my first Laravel app! 🚀',
-            'time' => '5 minutes ago'
-        ],
-        [
-            'author' => 'John Smith',
-            'message' => 'Laravel makes web development fun again!',
-            'time' => '1 hour ago'
-        ],
-        [
-            'author' => 'Alice Johnson',
-            'message' => 'Working on something cool with Chirper...',
-            'time' => '3 hours ago'
-        ]
-
-        ];
+        $chirps = Chirp::with('user')
+        ->latest()
+        ->get();
 
         return view('home', ['chirps' => $chirps]);
 
